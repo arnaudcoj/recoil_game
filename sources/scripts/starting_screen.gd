e48b8cc2a647c9a20744dcd578d7ae2474c22fb8
Non0w <arnaud.cojez@etudiant.univ-lille1.fr>
2016-10-16 12:57:56 +0200

    add a nice font to the GUI

--- sources/scripts/starting_screen.gd
+++ sources/scripts/starting_screen.gd
@@ -5,6 +5,11 @@ signal start_pressed
 
 func _ready():
 	set_process_input(true)
+	print()
+	print(exp(2))
+	print(exp(3))
+	print(exp(4))
+	print(exp(5))
 
 func _input(event):
 	if event.is_action_pressed("p1_start") or event.is_action_pressed("p2_start") or event.is_action_pressed("p3_start") or event.is_action_pressed("p4_start"):
